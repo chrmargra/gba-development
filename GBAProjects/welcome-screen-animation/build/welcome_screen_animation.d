d21a34282dacfe01266e785eb9c3bd839cdeaa2e
Christian <123153172+chrmargra@users.noreply.github.com>
2026-06-26 21:18:05 +0200

    Add save file for welcome screen music project

---
+++ GBAProjects/welcome-screen-animation/build/welcome_screen_animation.d
@@ -0,0 +1,18 @@
+welcome_screen_animation.o: \
+ /Users/christian/Projects/gba-development/GBAProjects/welcome-screen-animation/source/welcome_screen_animation.c \
+ /opt/devkitpro/libgba/include/gba_video.h \
+ /opt/devkitpro/libgba/include/gba_base.h \
+ /opt/devkitpro/libgba/include/gba_types.h \
+ /opt/devkitpro/libgba/include/gba_systemcalls.h \
+ /opt/devkitpro/libgba/include/gba_interrupt.h \
+ /opt/devkitpro/libgba/include/gba_dma.h \
+ /opt/devkitpro/libgba/include/gba_types.h \
+ /Users/christian/Projects/gba-development/GBAProjects/welcome-screen-animation/source/image.h
+/opt/devkitpro/libgba/include/gba_video.h:
+/opt/devkitpro/libgba/include/gba_base.h:
+/opt/devkitpro/libgba/include/gba_types.h:
+/opt/devkitpro/libgba/include/gba_systemcalls.h:
+/opt/devkitpro/libgba/include/gba_interrupt.h:
+/opt/devkitpro/libgba/include/gba_dma.h:
+/opt/devkitpro/libgba/include/gba_types.h:
+/Users/christian/Projects/gba-development/GBAProjects/welcome-screen-animation/source/image.h:
--- GBAProjects/welcome-screen-music/build/welcome_screen_music.d
+++ GBAProjects/welcome-screen-music/build/welcome_screen_music.d
@@ -1,5 +1,5 @@
-welcome_screen.o: \
- /Users/christian/Projects/gba-development/GBAProjects/welcome-screen-animation/source/welcome_screen.c \
+welcome_screen_music.o: \
+ /Users/christian/Projects/gba-development/GBAProjects/welcome-screen-music/source/welcome_screen_music.c \
  /opt/devkitpro/libgba/include/gba_video.h \
  /opt/devkitpro/libgba/include/gba_base.h \
  /opt/devkitpro/libgba/include/gba_types.h \
@@ -7,7 +7,7 @@ welcome_screen.o: \
  /opt/devkitpro/libgba/include/gba_interrupt.h \
  /opt/devkitpro/libgba/include/gba_dma.h \
  /opt/devkitpro/libgba/include/gba_types.h \
- /Users/christian/Projects/gba-development/GBAProjects/welcome-screen-animation/source/image.h
+ /Users/christian/Projects/gba-development/GBAProjects/welcome-screen-music/source/image.h
 /opt/devkitpro/libgba/include/gba_video.h:
 /opt/devkitpro/libgba/include/gba_base.h:
 /opt/devkitpro/libgba/include/gba_types.h:
@@ -15,4 +15,4 @@ welcome_screen.o: \
 /opt/devkitpro/libgba/include/gba_interrupt.h:
 /opt/devkitpro/libgba/include/gba_dma.h:
 /opt/devkitpro/libgba/include/gba_types.h:
-/Users/christian/Projects/gba-development/GBAProjects/welcome-screen-animation/source/image.h:
+/Users/christian/Projects/gba-development/GBAProjects/welcome-screen-music/source/image.h:
